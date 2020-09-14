5c94120fcee9e250d63fd8a239a5aecef5b615c6
mmanc125uw <mmanc125@uw.edu>
2020-09-14 11:03:30 -0400

    sp_py230 lesson3, final refactor, gen results.txt

--- do_unit_tests.bat
+++ do_unit_tests.bat
@@ -1,6 +1,5 @@
-python -m unittest -vv unit-tests.py
-rem  > unit_test_results.txt
-rem type unit_test_results.txt
+python -m unittest -vv unit-tests.py > unit_test_results.txt 2>&1
+type unit_test_results.txt
 
 
 
--- http_server.py
+++ http_server.py
@@ -58,7 +58,6 @@ def parse_request(request):
     if method != "GET":
         raise NotImplementedError
 
-    print("***MMM parse_request path= " + path)
     return path
 
 def response_path(path):
@@ -89,107 +88,60 @@ def response_path(path):
 
     """
 
-    print("aaa")
-
     content = b"not implemented"
     mime_type = b"not implemented"
     
-    print("bbb")
-    
     fps = ('webroot\\' + path)
-    print("response_path fps = " + str(fps))
-    
     status_isfile = os.path.isfile('webroot\\' + path)
-    print("response_path isfile status = " + str(status_isfile))
-
-    print("ccc")
-
     status_isdir = os.path.isdir('webroot\\' + path)
-    print("response_path isdir status = " + str(status_isdir))
-
-    print("ddd")
 
     # DONE: Raise a NameError if the requested content is not present
     # under webroot.
     if ( (status_isfile == False) and (status_isdir == False) ):
-        print("raise nameerror")
         raise NameError
 
     if (status_isfile):
 
         mime_type=b"text/plain"
         
-        print("***MMM resource is a file fps =" + fps)
-        
-      
         filename, file_extension = os.path.splitext(fps)
-        print(f"***MMM filename={filename}, extenstion={file_extension}")
-
         if (file_extension == ".html"):
             f=open(fps, "r")
             contents =f.read()
             f.close()
-            print("***MMM text file contents =" + str(contents))
             content=contents.encode('utf-8')
             mime_type=b"text/html"
         elif (file_extension == ".txt"):
             f=open(fps, "r")
             contents =f.read()
             f.close()
-            #print("***MMM text file contents =" + str(contents))
             content=contents.encode('utf-8')
             mime_type=b"text/plain"
         elif (file_extension == ".png"):
-            print("2222222222 binary file read")
-
             f=open(fps,"rb")
-            #ary_bytes=list(f.read())
-            #contents = ""
-            #contents = ''.join([str(elem) for elem in ary_bytes])
             ary_bytes = f.read()
             contents = ary_bytes
-            print("***MMM binary file contents =" + str(contents))
-            #content=contents.encode('utf-8')
             content=contents
             mime_type=b"image/png"
-            
-            #contents = base64.b64encode(open(fps, 'rb').read()).decode('utf-8')
-            #content = contents
-            #mime_type=b"image/png;base64"
-
-            #f = open(fps, "rb")
-            #byte = f.read(1)
-            #while byte:
-            #    print(byte)
-            #    byte = f.read(1)
-
-            
-
             f.close()
         elif (file_extension == ".jpg"):
             f=open(fps,"rb")
             ary_bytes = f.read()
             contents = ary_bytes
-            #print("***MMM binary file contents =" + str(contents))
             content=contents
             mime_type=b"image/jpeg"
             f.close()
 
     elif (status_isdir):
         ary_files = os.listdir(fps)
-        print(f"***MMM list of files in dir {ary_files}")
         str_files = '\n'.join(ary_files)
         content = str_files.encode('utf-8')
         mime_type=b"text/plain"
     
-
-    print("***MMM file content encoded =" + str(content))
-        
     # Done: Fill in the appropriate content and mime_type give the path.
     # See the assignment guidelines for help on "mapping mime-types", though
     # you might need to create a special case for handling make_time.py
         
-
     return content, mime_type
 
 
@@ -197,7 +149,7 @@ def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print("making a server on {0}:{1}".format(*address), file=log_buffer)
+    #print("making a server on {0}:{1}".format(*address), file=log_buffer)
     sock.bind(address)
     sock.listen(1)
 
@@ -210,18 +162,13 @@ def server(log_buffer=sys.stderr):
 
                 request = ''
                 while True:
-                    #print("11111")
                     data = conn.recv(1024)
-                    #print("22222")
                     request += data.decode('utf8')
 
                     if '\r\n\r\n' in request:
-                        #print("55555")
                         break
 
-                    #print("44444")
-
-                print("Request received:\n{}\n\n".format(request))
+                #print("Request received:\n{}\n\n".format(request))
 
                 try:
                 
@@ -235,21 +182,12 @@ def server(log_buffer=sys.stderr):
 
                         # DONE: Use response_path to retrieve the content and the mimetype,
                         # based on the request path.
-                        print("call response_path")
                         content, mime_type = response_path(path)
 
-                        #print("***MMM response content encoded =" + str(content))
-                        print("***MMM 1111111111111111111111111")
-                        
-                        
-                        
                         # DONE: If no exceptions use the content and mimetype from response_path to build a 
                         # response_ok.
                         response = response_ok(content, mime_type)
                         
-                        print("***MMM ***********************")
-                        #print("88888 response = " + str(response))
-                    
                 except NameError:
                     # DONE; If response_path raised
                     # a NameError, then let response be a not_found response. Else,
@@ -264,7 +202,6 @@ def server(log_buffer=sys.stderr):
             except:
                 traceback.print_exc()
             finally:
-                print("***MMM closing connection")
                 conn.close() 
                 
     except KeyboardInterrupt:
