668f828a6455ad110dc1890cd433571078b991fd
stellie <kims82@gmail.com>
2020-12-09 12:42:24 -0800

    setup assignment, update response ok, response method not allowed, parse request functions

--- http_server.py
+++ http_server.py
@@ -1,8 +1,12 @@
+# Stella Kim
+# Practice Activity 3: Web Protocols
+
 import socket
 import sys
 import traceback
 
-def response_ok(body=b"This is a minimal response", mimetype=b"text/plain"):
+
+def response_ok(body=b'This is a minimal response', mimetype=b'text/plain'):
     """
     returns a basic HTTP response
     Ex:
@@ -20,20 +24,30 @@ def response_ok(body=b"This is a minimal response", mimetype=b"text/plain"):
     """
 
     # TODO: Implement response_ok
-    return b""
+    return b'\r\n'.join([
+        b'HTTP/1.1 200 OK',
+        b'Content-Type: ' + mimetype,
+        b'',
+        body,
+    ])
+
 
 def response_method_not_allowed():
     """Returns a 405 Method Not Allowed response"""
 
     # TODO: Implement response_method_not_allowed
-    return b""
+    return b'\r\n'.join([
+        b'HTTP/1.1 405 Method Not Allowed',
+        b'',
+        b'You cannot do that on this server!'
+    ])
 
 
 def response_not_found():
     """Returns a 404 Not Found response"""
 
     # TODO: Implement response_not_found
-    return b""
+    return b''
 
 
 def parse_request(request):
@@ -45,7 +59,13 @@ def parse_request(request):
     """
 
     # TODO: implement parse_request
-    return ""
+    method, path, version = request.split('\r\n')[0].split(' ')
+
+    if method != 'GET':
+        raise NotImplementedError
+
+    return path
+
 
 def response_path(path):
     """
@@ -85,9 +105,9 @@ def response_path(path):
     # If the path is "make_time.py", then you may OPTIONALLY return the
     # result of executing `make_time.py`. But you need only return the
     # CONTENTS of `make_time.py`.
-    
-    content = b"not implemented"
-    mime_type = b"not implemented"
+
+    content = b'not implemented'
+    mime_type = b'not implemented'
 
     return content, mime_type
 
@@ -96,7 +116,7 @@ def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print("making a server on {0}:{1}".format(*address), file=log_buffer)
+    print('making a server on {0}:{1}'.format(*address), file=log_buffer)
     sock.bind(address)
     sock.listen(1)
 
@@ -114,24 +134,28 @@ def server(log_buffer=sys.stderr):
 
                     if '\r\n\r\n' in request:
                         break
-		
 
-                print("Request received:\n{}\n\n".format(request))
+                print('Request received:\n{}\n\n'.format(request))
 
-                # TODO: Use parse_request to retrieve the path from the request.
+                try:
+                    path = parse_request(request)
 
-                # TODO: Use response_path to retrieve the content and the mimetype,
-                # based on the request path.
+                    # TODO: Use parse_request to retrieve the path from the request.
 
-                # TODO; If parse_request raised a NotImplementedError, then let
-                # response be a method_not_allowed response. If response_path raised
-                # a NameError, then let response be a not_found response. Else,
-                # use the content and mimetype from response_path to build a 
-                # response_ok.
-                response = response_ok(
-                    body=b"Welcome to my web server",
-                    mimetype=b"text/plain"
-                )
+                    # TODO: Use response_path to retrieve the content and the mimetype,
+                    # based on the request path.
+
+                    # TODO; If parse_request raised a NotImplementedError, then let
+                    # response be a method_not_allowed response. If response_path raised
+                    # a NameError, then let response be a not_found response. Else,
+                    # use the content and mimetype from response_path to build a 
+                    # response_ok.
+                    response = response_ok(
+                        body=b'Welcome to my web server',
+                        mimetype=b'text/plain'
+                    )
+                except NotImplementedError:
+                    response = response_method_not_allowed()
 
                 conn.sendall(response)
             except:
@@ -149,5 +173,3 @@ def server(log_buffer=sys.stderr):
 if __name__ == '__main__':
     server()
     sys.exit(0)
-
-
